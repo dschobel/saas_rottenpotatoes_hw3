7498d197230b25bc1441bc20a664c03432c873d1
Daniel Schobel <dschobel@gmail.com>
2012-09-10 08:00:15 -0700

    finished sort feature

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,5 +1,5 @@
 Then /I should see all of the movies/ do
-  #account for header
+  #account for header by subtracting 1 from count
   (page.all('table#movies tr').count - 1).should == Movie.all.count
 end
 
@@ -10,18 +10,12 @@ Given /the following movies exist/ do |movies_table|
   end
 end
 
-# Make sure that one string (regexp) occurs before or after another one
-#   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
-  pending "Unimplemented"
+  one = (page.body =~ /#{Regexp.quote(e1)}/)
+  two = (page.body =~ /#{Regexp.quote(e2)}/)
+  one.should be < two
 end
-
-# Make it easier to express checking or unchecking several boxes at once
-#  "When I uncheck the following ratings: PG, G, R"
-#  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   rating_list.split(',').each do |rating|
@@ -31,7 +25,4 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
       check "ratings_#{rating.strip}"
     end
   end
-# HINT: use String#split to split up the rating_list, then
-#   iterate over the ratings and reuse the "When I check..." or
-#   "When I uncheck..." steps in lines 89-95 of web_steps.rb
 end
